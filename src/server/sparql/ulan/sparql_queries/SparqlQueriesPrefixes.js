d785b7a01dab538595289c9d2756735caa7003cb
HeikkiR <heikki.rantala@aalto.fi>
2023-08-09 21:19:03 +0300

    Fix persons perspective

--- src/server/sparql/ulan/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/ulan/sparql_queries/SparqlQueriesPrefixes.js
@@ -2,8 +2,6 @@ export const prefixes = `
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX crm: <http://erlangen-crm.org/current/>
-  PREFIX crm-org: <http://www.cidoc-crm.org/cidoc-crm/>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
   PREFIX text: <http://jena.apache.org/text#>
@@ -16,12 +14,15 @@ export const prefixes = `
   PREFIX gvp: <http://vocab.getty.edu/ontology#>
   PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
   PREFIX spatial: <http://jena.apache.org/spatial#>
-  PREFIX text: <http://jena.apache.org/text#>
   PREFIX sd: <http://www.w3.org/ns/sparql-service-description#>
   PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
   PREFIX h-schema: <http://ldf.fi/schema/hellerau/> 
   PREFIX gn: <http://www.geonames.org/ontology#>
   PREFIX rel: <http://ldf.fi/schema/relations/>
   PREFIX schema: <http://schema.org/>
+  prefix ulan: <http://vocab.getty.edu/ulan/>
+  prefix xl: <http://www.w3.org/2008/05/skos-xl#>
+  PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+  PREFIX aat: <http://vocab.getty.edu/aat/>
   
 `
